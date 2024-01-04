6900df4930f918f058745a151159ed9e22e79c39
buihien0109 <buihien01091997@gmail.com>
2024-01-04 20:40:17 +0700

    update

--- springboot/day_03/demo-stream-api/src/main/java/org/example/demo/stream/api/controller/PersonController.java
+++ springboot/day_03/demo-stream-api/src/main/java/org/example/demo/stream/api/controller/PersonController.java
@@ -1,32 +1,42 @@
 package org.example.demo.stream.api.controller;
 
+import org.example.demo.stream.api.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonController {
+    @Autowired
+    private PersonDAO personDAO;
+
     @GetMapping("/")
     public String getHome() {
         return "index";
     }
 
     @GetMapping("/getAll")
-    public String getAll() {
+    public String getAll(Model model) {
+        model.addAttribute("people", personDAO.getAll());
         return "getAll";
     }
 
     @GetMapping("/sortPeopleByFullName")
-    public String sortPeopleByFullName() {
+    public String sortPeopleByFullName(Model model) {
+        model.addAttribute("people", personDAO.sortPeopleByFullName());
         return "sortPeopleByFullName";
     }
 
     @GetMapping("/getSortedJobs")
-    public String getSortedJobs() {
+    public String getSortedJobs(Model model) {
+        model.addAttribute("jobs", personDAO.getSortedJobs());
         return "getSortedJobs";
     }
 
     @GetMapping("/getSortedCities")
-    public String getSortedCities() {
+    public String getSortedCities(Model model) {
+        model.addAttribute("cities", personDAO.getSortedCities());
         return "getSortedCities";
     }
 }
