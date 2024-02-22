48c883a05be1da8217a4d6b30ab7878562a07be2
buihien0109 <buihien01091997@gmail.com>
2024-02-22 21:54:26 +0700

    update

--- springboot/day_26/demo-thymeleaf-spring-security/src/main/java/vn/techmaster/demothymeleafspringsecurity/controller/WebController.java
+++ springboot/day_26/demo-thymeleaf-spring-security/src/main/java/vn/techmaster/demothymeleafspringsecurity/controller/WebController.java
@@ -2,6 +2,8 @@ package vn.techmaster.demothymeleafspringsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import vn.techmaster.demothymeleafspringsecurity.model.anotation.IsAdmin;
+import vn.techmaster.demothymeleafspringsecurity.model.anotation.IsUser;
 
 @Controller
 public class WebController {
@@ -12,12 +14,16 @@ public class WebController {
     }
 
     // Có role USER mới được truy cập
+    // @PreAuthorize("hasRole('USER')")
+    @IsUser
     @GetMapping("/profile")
     public String getProfile() {
         return "profile";
     }
 
     // Có role ADMIN mới được truy cập
+    // @PreAuthorize("hasRole('ADMIN')")
+    @IsAdmin
     @GetMapping("/admin")
     public String getAdmin() {
         return "admin";
--- springboot/day_26/demo-thymeleaf-spring-security/src/main/java/vn/techmaster/demothymeleafspringsecurity/security/SecurityConfig.java
+++ springboot/day_26/demo-thymeleaf-spring-security/src/main/java/vn/techmaster/demothymeleafspringsecurity/security/SecurityConfig.java
@@ -5,14 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
@@ -31,16 +35,17 @@ public class SecurityConfig {
         // Cấu hình đường dẫn , 401, 403
         String[] publicPath = {"/phim-le", "/phim-bo", "/phim-chieu-rap", "/tin-tuc"};
         http.authorizeHttpRequests(authorizeRequests -> {
-            authorizeRequests.requestMatchers("/").permitAll(); // Ai cũng được truy cập
-            authorizeRequests.requestMatchers("/css/**", "/js/**", "/image/**").permitAll(); // Các đường dẫn static
-            authorizeRequests.requestMatchers(publicPath).permitAll(); // Các đường dẫn public
-            authorizeRequests.requestMatchers("/profile").hasRole("USER"); // Có role USER mới được truy cập
-            authorizeRequests.requestMatchers("/admin").hasRole("ADMIN"); // Có role ADMIN mới được truy cập
-            authorizeRequests.requestMatchers("/author").hasAnyRole("ADMIN", "AUTHOR"); // Có role ADMIN hoặc AUTHOR mới được truy cập
-            authorizeRequests.requestMatchers("/users", "/movies").hasAuthority("ROLE_ADMIN"); // Có authority ROLE_ADMIN mới được truy cập
-            authorizeRequests.requestMatchers("/khach-hang", "/don-hang").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN"); // Có authority ROLE_USER hoặc ROLE_ADMIN mới được truy cập
-            authorizeRequests.requestMatchers("GET", "/aa/**", "/bb/**").hasRole("ADMIN"); // Có role ADMIN mới được truy cập
-            authorizeRequests.anyRequest().authenticated(); // Tất cả các request khác đều cần xác thực
+//            authorizeRequests.requestMatchers("/").permitAll(); // Ai cũng được truy cập
+//            authorizeRequests.requestMatchers("/css/**", "/js/**", "/image/**").permitAll(); // Các đường dẫn static
+//            authorizeRequests.requestMatchers(publicPath).permitAll(); // Các đường dẫn public
+//            authorizeRequests.requestMatchers("/profile").hasRole("USER"); // Có role USER mới được truy cập
+//            authorizeRequests.requestMatchers("/admin").hasRole("ADMIN"); // Có role ADMIN mới được truy cập
+//            authorizeRequests.requestMatchers("/author").hasAnyRole("ADMIN", "AUTHOR"); // Có role ADMIN hoặc AUTHOR mới được truy cập
+//            authorizeRequests.requestMatchers("/users", "/movies").hasAuthority("ROLE_ADMIN"); // Có authority ROLE_ADMIN mới được truy cập
+//            authorizeRequests.requestMatchers("/khach-hang", "/don-hang").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN"); // Có authority ROLE_USER hoặc ROLE_ADMIN mới được truy cập
+//            authorizeRequests.requestMatchers("GET", "/aa/**", "/bb/**").hasRole("ADMIN"); // Có role ADMIN mới được truy cập
+//            authorizeRequests.anyRequest().authenticated(); // Tất cả các request khác đều cần xác thực
+            authorizeRequests.anyRequest().permitAll();
         });
 
         // Cấu hình form login
